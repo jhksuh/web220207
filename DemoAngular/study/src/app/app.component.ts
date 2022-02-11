c24422d366dd42385cf227128338ac7e3f38bf1b
jhksuh <add2608@gmail.com>
2022-02-11 11:13:58 +0900

    node 연습

--- DemoAngular/study/src/app/app.component.ts
+++ DemoAngular/study/src/app/app.component.ts
@@ -18,11 +18,21 @@ export class AppComponent {
     console.log(arg);
   }
 
+  public ifIwereGoToServer() : void {  //요 메소드이다.
+    //어떤 특정 데이터를 주는 서버에 갔다왔다고 하자.
+    //예를들어 ajax를 실행하고 나서 result값이 아래와 같은 배열이 나왔다고 가정하자.
+    this.all.push('Result');
+    this.all.push(12345);
+    this.all.push(23456);
+    this.all.push(78923);
+  }
+
+
   constructor() {
     var text : string = 'plain text';
     this.print(text);
     this.printAll(text);
-    
+
     console.log(this.title, this.num, this.all);  //멤버들도 this를 써 주어야 한다.
   }
 }
